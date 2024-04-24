0e9e88f57e1f5ab7c7fea9c6fe1ebfde1cc71792
Nicholas Morey <nicholas@morey.tech>
2024-04-24 22:39:10 +0000

    updated source to point to morey-tech/intro-argo-cd-kargo-demo

--- .devcontainer/update-repo-for-workshop.sh
+++ .devcontainer/update-repo-for-workshop.sh
@@ -25,18 +25,73 @@ done
 ## Now that the files are updated, we commit it and push it up. Best effort :cross_fingers_emoji:
 cd ${workspace}
 git add .
-git commit -am "updated source to point to ${GITHUB_USER}"
+git commit -am "updated source to point to ${GITHUB_REPOSITORY}"
 git push origin main
 
+## Update the freight placeholders
 COMMIT_SHA=$(git rev-parse HEAD)
-find ${workspace} -name '*.yaml' -type f -exec grep -l '<commit-sha>' {} \; | while read file
-do
-    sed -i "s?<commit-sha>?${COMMIT_SHA}?g" ${file}
-done
-find ${workspace} -name '*.yaml' -type f -exec grep -l '<commit-msg>' {} \; | while read file
-do
-    sed -i "s?<commit-msg>?updated source to point to ${GITHUB_USER}?g" ${file}
-done
+
+kubectl apply -f - <<EOF
+alias: flying-monkey
+apiVersion: kargo.akuity.io/v1alpha1
+commits:
+- branch: main
+  id: ${COMMIT_SHA}
+  message: 'updated source to point to ${GITHUB_REPOSITORY}'
+  repoURL: https://github.com/${GITHUB_REPOSITORY}
+images:
+- digest: sha256:57f27650e10ae33978bc14c80c90b7688ecad3950b308d1ec157c9495ebcefc4
+  repoURL: quay.io/akuity/argo-cd-learning-assets/guestbook
+  tag: 0.1.0
+kind: Freight
+metadata:
+  labels:
+    kargo.akuity.com/v0.5-compatible: "true"
+    kargo.akuity.io/alias: flying-monkey
+  name: placeholder-0.1.0
+  namespace: kargo-demo
+warehouse: kargo-demo
+---
+alias: sprinting-chipmunk
+apiVersion: kargo.akuity.io/v1alpha1
+commits:
+- branch: main
+  id: ${COMMIT_SHA}
+  message: 'updated source to point to ${GITHUB_REPOSITORY}'
+  repoURL: https://github.com/${GITHUB_REPOSITORY}
+images:
+- digest: sha256:57f27650e10ae33978bc14c80c90b7688ecad3950b308d1ec157c9495ebcefc4
+  repoURL: quay.io/akuity/argo-cd-learning-assets/guestbook
+  tag: 0.2.0
+kind: Freight
+metadata:
+  labels:
+    kargo.akuity.com/v0.5-compatible: "true"
+    kargo.akuity.io/alias: sprinting-chipmunk
+  name: placeholder-0.2.0
+  namespace: kargo-demo
+warehouse: kargo-demo
+---
+alias: screaming-dolphin
+apiVersion: kargo.akuity.io/v1alpha1
+commits:
+- branch: main
+  id: ${COMMIT_SHA}
+  message: 'updated source to point to ${GITHUB_REPOSITORY}'
+  repoURL: https://github.com/${GITHUB_REPOSITORY}
+images:
+- digest: sha256:57f27650e10ae33978bc14c80c90b7688ecad3950b308d1ec157c9495ebcefc4
+  repoURL: quay.io/akuity/argo-cd-learning-assets/guestbook
+  tag: 0.3.0
+kind: Freight
+metadata:
+  labels:
+    kargo.akuity.com/v0.5-compatible: "true"
+    kargo.akuity.io/alias: screaming-dolphin
+  name: placeholder-0.3.0
+  namespace: kargo-demo
+warehouse: kargo-demo
+EOF
 
 ## Exit with 0 for the post-start script
 exit 0

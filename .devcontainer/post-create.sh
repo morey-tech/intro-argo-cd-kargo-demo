c97c8bf19d2b995edc680210b316184bbf3523c7
Kargo Render <kargo-render@akuity.io>
2024-04-24 12:47:45 +0000

    Kargo applied some changes

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## Log start
+echo "post-create start" >> ~/.status.log
+
+## Install the things
+###curl -L https://raw.githubusercontent.com/akuity/kargo/main/hack/quickstart/kind.sh | bash
+echo "=================" >> ~/.status.log
+echo "= Bootstrapping =" >> ~/.status.log
+echo "=================" >> ~/.status.log
+bash .devcontainer/bootstrap-workloads.sh | tee -a ~/.status.log
+
+## Update Repo With proper username
+bash .devcontainer/update-repo-for-workshop.sh | tee -a ~/.status.log
+
+## Log things
+echo "post-create complete" >> ~/.status.log
+echo "--------------------" >> ~/.status.log
+
+##

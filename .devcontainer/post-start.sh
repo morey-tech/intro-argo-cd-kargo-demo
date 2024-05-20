12853dd48e888036d904f477329a437dabf953ea
Kargo Render <kargo-render@akuity.io>
2024-05-20 23:59:13 +0000

    Kargo applied some changes

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -8,17 +8,18 @@ kind export kubeconfig --name kargo-quickstart
 
 ## Kargo, login and create project
 kargo login --admin --insecure-skip-tls-verify --password admin https://localhost:31444
-kargo create project kargo-demo
-kargo create credentials \
-    --project=kargo-demo kargo-demo-repo \
-    --git --repo-url=https://github.com/${GITHUB_REPOSITORY} \
-    --username=${GITHUB_USER} --password=${GITHUB_TOKEN}
 
 ## Argo CD, login
 argocd login --username admin --password admin --insecure --grpc-web localhost:31443
 
 ## Apply Manifests
 kubectl apply -k kargo/
+
+# kargo create project kargo-demo
+kargo create credentials \
+    --project=kargo-demo kargo-demo-repo \
+    --git --repo-url=https://github.com/${GITHUB_REPOSITORY} \
+    --username=${GITHUB_USER} --password=${GITHUB_TOKEN}
 
 ## Pre-populate freightline
 bash .devcontainer/populate-freightline.sh | tee -a ~/.status.log
